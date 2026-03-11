ea466f5c012b841bafeb761ecb8dc07a3b371421
wumingxin238 <1208399599@qq.com>
2026-03-11 21:58:18 +0800

    Fix FilterCommand class

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -13,7 +13,6 @@ public class FilterCommand extends Command{
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        
         return null;
     }
 
