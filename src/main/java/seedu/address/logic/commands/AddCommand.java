8ac9f868d9ee7ac1921943c9f3537c2caa308a0c
SeePui <ngseepui95@gmail.com>
2026-03-21 02:49:40 +0800

    Remove extra spacing for add command message usage

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -30,7 +30,7 @@ public class AddCommand extends Command {
             + PREFIX_NAME + "John Doe "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_PHONE + "98765432 "
-            + PREFIX_TELEGRAM_HANDLE + "johndoe123 ";
+            + PREFIX_TELEGRAM_HANDLE + "johndoe123";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "A person with this email already exists in the address book";
