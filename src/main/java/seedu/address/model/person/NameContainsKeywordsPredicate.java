c4e2e8f07916e1d226af3c8f231f5fd8764fa24b
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-04-01 13:21:30 +0800

    Add fuzzy match for name keywords

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -2,15 +2,21 @@ package seedu.address.model.person;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
+import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
  */
 public class NameContainsKeywordsPredicate implements Predicate<Person> {
+
+    private static final int MIN_EDIT_RATIO = 1;
+    private static final double EDIT_DISTANCE_RATIO = 0.2;
+
     private final List<String> keywords;
 
     /**
@@ -26,10 +32,23 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
 
     @Override
     public boolean test(Person person) {
+        // Split name based on white spaces
+        String[] nameTokens = person.getName().fullName
+                .replaceAll("[^a-zA-Z0-9\\s]", "") // Remove punctuation
+                .toLowerCase()
+                .split("\\s+");
+
         return keywords.stream()
-                .anyMatch(keyword -> person.getName().fullName
-                        .toLowerCase()
-                        .contains(keyword.toLowerCase()));
+                .map(String::toLowerCase)
+                .anyMatch(keyword -> Arrays.stream(nameTokens)
+                        .anyMatch(token -> {
+                            if (token.contains(keyword)) {
+                                return true;
+                            }
+
+                            int threshold = Math.max(MIN_EDIT_RATIO, (int) (keyword.length() * EDIT_DISTANCE_RATIO));
+                            return StringUtil.matchesFuzzy(token, keyword, threshold);
+                        }));
     }
 
     @Override
